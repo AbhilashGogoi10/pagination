150075a96bbeac53fb72f5922b6d48513a38890f
AbhilashGogoi10 <abhilashgogoi33@gmail.com>
2023-10-13 20:09:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0)
+
+
+    const fetchProducts = async()=>{
+        const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${page * 10-10}`);
+        const data = await res.json();
+
+        if(data && data.products){
+            setProducts(data.products)
+            setTotalPages(data.total/10);
+        }
+    }
+
+    useEffect(()=>{
+        fetchProducts();
+    },[page])
+
+    const selectPageHandler=(selectedPage)=>{
+        if(selectedPage>=1 && selectedPage<=totalPages && selectedPage !== page )
+        setPage(selectedPage);
+        
+    }
+
+    return(
+        <div >
+            {totalPages > 0 && (
+                <div className="products">
+                    {products.map((prod)=>{
+                        return <span className="products__single" key={prod.id}>
+                            <img src={prod.thumbnail} alt={prod.title} />
+                            <span>{prod.title}</span>
+                        </span>
+                    })}
+                </div>
+            ) }
+
+            {
+                totalPages > 0 && (
+                    <div className="pagination">
+                        <span 
+                        className={page === 1 ? "pagination__disable" : ""}
+                        onClick={()=>selectPageHandler(page-1)}>⏪</span>
+                        {
+                            [...Array(totalPages)].map((_,i)=>{
+                                return <span
+                                className={page === i+1 ? "pagination__selected":""}
+                                 onClick={()=>selectPageHandler(i+1)} key={i}>{i+1}</span>
+                            })
+                        }
+                        <span onClick={()=>selectPageHandler(page+1)}
+                        className={page < totalPages ? "" : "pagination__disable"}>⏩</span>
+                    </div>
+                )
+            }
+        </div>
+    )
+
 }
 
 export default App;
